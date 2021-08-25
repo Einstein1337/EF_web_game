3a030ccb472b9f3f005d408346c0a49f9afcc3b6
Einstein1337 <pacifico4@gmx.ch>
2021-08-25 22:25:19 +0200

    Added winning detection

--- connect_four.js
+++ connect_four.js
@@ -15,6 +15,7 @@ class Cell
     {
         this.id = id
         this.used = false
+        this.color = ""
     }
 }
 
@@ -63,27 +64,90 @@ class Game
         return rows - 1;
     }
 
+    fieldEvaluation(last_token_row, last_token_column, last_color)
+    {
+        let i_j_list = [[-1, 1],[0, 1],[1, 1],[1, 0]];
+        for (let dir = 0; dir < 4; dir++)
+        {
+            let winning_index = 0;
+            let no_win = false;
+            let i = i_j_list[dir][0];
+            let j = i_j_list[dir][1];
+            for (let neighbour = 0; neighbour < 4; neighbour++)
+            {
+                let fail = false;
+                let next_row = last_token_row+i*(neighbour+1);
+                let next_column = last_token_column+j*(neighbour+1);
+
+                if(next_row <= rows-1 && next_column <= columns-1 && next_row >= 0 && next_column >= 0)
+                {
+                    if (this.cell_list[next_row][next_column].color != last_color)
+                    {
+                        fail = true
+                    }
+                    else
+                    {
+                        winning_index += 1;
+                    }
+                }
+                else
+                {
+                    fail = true
+                }
+
+                if (fail)
+                {
+                    if (no_win)
+                    {
+                        break
+                    }
+                    else
+                    {
+                        i *= -1;
+                        j *= -1;
+                        no_win == true;
+                    }
+                }
+
+                if (winning_index == 3)
+                {
+                    return true;
+                }
+            }    
+        }
+        return false;
+
+    }
 
     insertPiece(button)
     {
         let cell = this.findCell(button.id)
         let color = ''
+        let color_str = ""
         if (!cell.used)
         {
             cell.used = false
             if (document.getElementById("player").className == "player1")
             {
                 color = '#FF0000';
+                color_str = "red"
                 document.getElementById("player").className = "player2";
             }
             else
             {
                 color = '#FFFF00';
+                color_str = "yellow"
                 document.getElementById("player").className = "player1";
             }
             let free_row = this.findNearestFreeColumnPlace(parseInt(cell.id[1]))
-            this.cell_list[free_row][cell.id[1]].used = true
+            this.cell_list[free_row][parseInt(cell.id[1])].used = true
+            this.cell_list[free_row][parseInt(cell.id[1])].color = color_str
             document.getElementById(""+free_row+""+parseInt(cell.id[1])).style.background=color;//red
+            let win = this.fieldEvaluation(free_row, parseInt(cell.id[1]), color_str)
+            if(win)
+            {
+                console.log("Win")
+            }
         }
     }
 }
