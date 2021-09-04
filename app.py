dcac3b12390986791bf9b9522b34ff0b9d61d728
Einstein1337 <pacifico4@gmx.ch>
2021-09-04 13:59:42 +0200

    started HTTP, added new game Button

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import url_for
+from werkzeug.utils import redirect
+from flask import render_template
+from connect_four import *
+import random
+
+game_list = []
+next_game_id = 0
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('connect_four.html')#redirect(url_for('static', filename='connect_four.html'))
+
+@app.route("/newgame")
+def newGame():
+    if len(game_list) > 0:
+        for game in game_list:
+            if game.mode != "running":
+                game.Join()
+    else: 
+        game_list.append(Game(next_game_id))
+        next_game_id += 1
+        
---
+++ connect_four.py
@@ -0,0 +1,81 @@
+ROWS = 6
+COLUMNS = 7
+
+class Player:
+    def __init__(self, id, color):
+        self.id = id
+        self.color = color
+
+
+class Cell:
+    def __init__(self, id):
+        self.id = id
+        self.used = False
+        self.color = ""
+
+class Game:
+    def __init__(self, id):
+        self.id = 0
+        self.mode = "waiting"
+        self.player1 = Player(1, "red")
+        self.player2 = Player(2, "yellow")
+        self.currentPlayer = self.player1
+        self.cell_list = []
+
+        for r in range(ROWS):
+            self.cell_list.append([])
+            for c in range(COLUMNS):
+                self.cell_list[r].append(Cell(f"{r}{c}"))
+
+    def Join(self):
+        pass
+    
+
+    def fieldEvaluation(self, last_token_row, last_token_column, last_color):
+        i_j_list = [[-1, 1],[0, 1],[1, 1],[1, 0]]
+        for dir in range(4):
+            winning_index = 0
+            no_win = False
+            i = i_j_list[dir][0]
+            j = i_j_list[dir][1]
+            running_var = 1
+            for neighbour in range(4):
+                fail = False
+                next_row = last_token_row+i*running_var
+                next_column = last_token_column+j*running_var
+
+                if next_row <= ROWS-1 and next_column <= COLUMNS-1 and next_row >= 0 and next_column >= 0:
+                    if self.cell_list[next_row][next_column].color != last_color:
+                        fail = True
+                    else:
+                        winning_index += 1
+                else:
+                    fail = True
+
+                if fail:
+                    if no_win:
+                        break
+                    else:
+                        i *= -1
+                        j *= -1
+                        no_win == True
+                        running_var = 0
+
+                if winning_index == 3:
+                    return True
+                running_var += 1
+        return False
+
+    def move(self, column):
+        for row in range(ROWS):
+            if self.cell_list[row][column].used == True:
+                if row != 0:
+                    self.cell_list[row-1][column].used = True
+                    self.cell_list[row-1][column].color = self.currentPlayer.color
+
+                    if self.currentPlayer.id == 1:
+                        self.currentPlayer = self.player2
+                    else:
+                        self.currentPlayer = self.player1
+
+                    win = self.fieldEvaluation(row-1, column, self.cell_list[row-1][column].color)     
